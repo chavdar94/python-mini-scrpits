ea8508cf1ffee3a348abba6bb6e1aff1e45e17c7
Chavdar <ch.tonchev@gmail.com>
2022-12-05 08:32:32 +0200

    Changed file name

---
+++ pdftodocx.py
@@ -0,0 +1,9 @@
+from pdf2docx import parse
+
+# path to pdf file
+pdf_file = '/home/choko/Downloads/sample.pdf'
+# path to word file
+docx_file = '/home/choko/Downloads/sample.docx'
+
+
+parse(pdf_file, docx_file)
