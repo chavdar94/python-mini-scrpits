d5bba283033dbbc0f30824926cef34fd9896d2b1
chavdar tonchev <106109451+chavdar94@users.noreply.github.com>
2023-05-20 12:21:44 +0300

    Update pdftodocx.py

--- pdftodocx.py
+++ pdftodocx.py
@@ -1,9 +1,9 @@
 from pdf2docx import parse
 
 # path to pdf file
-pdf_file = '/home/choko/Downloads/sample.pdf'
+pdf_file = '/home/user/Downloads/sample.pdf'
 # path to word file
-docx_file = '/home/choko/Downloads/sample.docx'
+docx_file = '/home/cluser/Downloads/sample.docx'
 
 
 parse(pdf_file, docx_file)
